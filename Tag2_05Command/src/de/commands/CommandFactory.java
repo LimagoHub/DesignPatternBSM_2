b59699deb5bbe87c7748cc02c50ad1bc6b56c544
limagoHub <limagoHub@limago.de>
2020-03-31 14:10:02 +0200

    CommandFactory

--- Tag2_05Command/src/de/commands/CommandFactory.java
+++ Tag2_05Command/src/de/commands/CommandFactory.java
@@ -4,18 +4,24 @@ public class CommandFactory {
 	
 	public static Command createCommand(String eingabe) {
 		
-		Command retval = null;
-		String [] tokens = eingabe.split(" ");
-		
-		if(tokens[0].equals("Add")) {
-			retval = new AddCommand();
-			retval.parse(tokens);
-		} else if (tokens[0].equals("Print")) { 
-			retval = new PrintCommand();
-			retval.parse(tokens);
+		try {
+			Command retval = null;
+			String [] tokens = eingabe.split(" ");
+			
+			if(tokens[0].equals("Add")) {
+				retval = new AddCommand();
+				retval.parse(tokens);
+			} else if (tokens[0].equals("Print")) { 
+				retval = new PrintCommand();
+				retval.parse(tokens);
+			}
+			
+			return retval;
+		} catch (Throwable e) {
+			
+			e.printStackTrace();
+			return null;
 		}
-		
-		return retval;
 	}
 
 }
